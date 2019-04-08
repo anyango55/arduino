b42c2815d26758b623e3f4b48a2423b3d5defd22
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-04-08 16:02:47 +0300

    Add files via upload

---
+++ arduino/blink_pot.ino
@@ -0,0 +1,19 @@
+int REDLED = 5;
+int pinpoot = AO;
+int inputValue = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (REDLED, OUTPUT);
+pinMode (pinpot,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  inputValue = analogRead(pinpot);
+  digitalWrite(REDLED,HIGH);
+  delay(inputValue);
+  digitalWrite(REDLED,LOW);
+  delay(inputValue);
+}
