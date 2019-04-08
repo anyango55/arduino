583e076c9f23cf56555a3113dac6df28770d8772
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-04-08 16:00:35 +0300

    Add files via upload

---
+++ arduino/arduino2.ino
@@ -0,0 +1,16 @@
+int REDLED = 10;
+
+void setup() {
+ pinMode(REDLED, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  analogWrite(REDLED,102);
+  delay(1000);
+  analogWrite(REDLED,0);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
