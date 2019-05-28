4541bafe0b703fe75d2c9748301732087d6cba2e
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-05-28 11:43:06 +0300

    Add files via upload

---
+++ RGB_LED.ino
@@ -0,0 +1,19 @@
+int redPin=11;
+int greenPin=6;
+int bluePin=6;
+int brightness=25;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(redPin, OUTPUT);
+pinMode(greenPin, OUTPUT);
+pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(redPin, 0);
+analogWrite(greenPin,100);
+analogWrite(bluePin,0);
+}
